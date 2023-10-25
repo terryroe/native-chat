dc078f028d3ac9bea79a261555f88464d544b864
Terry Roe <terryroe@gmail.com>
2023-10-25 15:19:55 -0600

    make app work for both Android and iOS, and videos

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,26 @@
-import { Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const Chat = () => {
+const Chat = ({ route, navigation }) => {
+  const { name, background } = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({ title: name || 'No Name Specified' });
+  }, []);
+
   return (
-    <View>
-      <Text>Chat</Text>
+    <View style={[styles.container, { backgroundColor: background }]}>
+      <Text style={{ color: 'white' }}>Chat</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 export default Chat;
--- components/Start.js
+++ components/Start.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import {
-  Button,
   ImageBackground,
   StyleSheet,
   Text,
@@ -10,9 +9,11 @@ import {
 } from 'react-native';
 
 const image = require('../assets/background.png');
+const colors = ['#090C08', '#474056', '#8A95A5', '#B9C6AE'];
 
 const Start = ({ navigation }) => {
   const [name, setName] = useState('');
+  const [background, setBackground] = useState(colors[0]);
 
   return (
     <ImageBackground source={image} style={styles.image}>
@@ -28,8 +29,24 @@ const Start = ({ navigation }) => {
         />
         <View style={styles.colorSelector}>
           <Text style={styles.colorTitle}>Choose Background Color:</Text>
+          <View style={styles.colorButtonWrapper}>
+            {colors.map((color) => (
+              <TouchableOpacity
+                key={color}
+                style={[
+                  styles.colorButton,
+                  { backgroundColor: color },
+                  background === color && styles.selected,
+                ]}
+                onPress={() => setBackground(color)}
+              />
+            ))}
+          </View>
         </View>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Chat', { name, background })}
+        >
           <Text style={styles.buttonText}>Start Chatting</Text>
         </TouchableOpacity>
       </View>
@@ -49,15 +66,29 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#fff',
     fontSize: 16,
-    fontWeight: 600,
+    fontWeight: '600',
+  },
+  colorButton: {
+    backgroundColor: 'white',
+
+    borderRadius: 25,
+    height: 50,
+    marginRight: 15,
+    marginTop: 10,
+    padding: 20,
+    width: 50,
+  },
+  colorButtonWrapper: {
+    flexDirection: 'row',
   },
   colorSelector: {
-    marginTop: -15,
+    flexWrap: 'wrap',
+    marginTop: -25,
+    width: '88%',
   },
   colorTitle: {
     color: '#757083',
     fontSize: 16,
-    textAlign: 'left',
   },
   contentWrapper: {
     alignItems: 'center',
@@ -72,11 +103,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-between',
   },
+  selected: {
+    borderColor: 'red',
+    borderWidth: 3,
+    padding: 5,
+  },
   title: {
     alignItems: 'center',
     color: '#fff',
     fontSize: 45,
-    fontWeight: 600,
+    fontWeight: '600',
     height: '44%',
   },
   titleWrapper: {
@@ -87,7 +123,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     color: '#757083',
     fontSize: 16,
-    fontWeight: 300,
+    fontWeight: '300',
     marginBottom: 20,
     marginTop: 20,
     padding: 20,
